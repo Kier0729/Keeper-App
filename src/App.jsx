1937b148e3279b11203353e03659036449d2c3bf
Kier Dalit <kier0729@gmail.com>
2024-01-11 19:52:21 +0800

    Finish Note Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Heading from "./Header";
+import Note from "./Note";
 
 function App() {
     return (
         <div>
             <Heading />
+            <Note />
         </div>
     );
 }
