feb10e0c50123511eb76b4bb32ddaa2a21572061
Kier Dalit <kier0729@gmail.com>
2024-01-15 17:30:20 +0800

    Finish adding Components/Props/ES6 Arrow Function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Heading from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes"
 
 function App() {
     return (
         <div>
             <Heading />
-            <Note />
+            {notes.map((data)=> <Note key = {data.key} title = {data.title} content={data.content}/>)}
             <Footer />
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(<App />);
